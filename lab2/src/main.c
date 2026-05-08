83a11dd7866c4bbde133850cf8a3171877a9b99c
Vedant Jhawar <vedantj1203@gmail.com>
2026-05-08 03:44:00 -0400

    finished lab 2 code, screenshots, logs

--- lab2/src/main.c
+++ lab2/src/main.c
@@ -7,49 +7,57 @@
 
 #define PERIOD_1S_CYCLES 8084000
 
+TickType_t last_wake; 
+
 void low_priority_task(void* pvParams) {
     char buf[64];
+    const TickType_t frequency_ticks = pdMS_TO_TICKS(3000);
+    TickType_t start_ticks = last_wake;
     uint32_t count = 0;
     while (1) {
         snprintf(buf, sizeof(buf), "[%lu ms] LOW %u Step 1\r\n", xTaskGetTickCount(),  count);
         uart_outstring(buf);
-        burn_cycles(8000000);
+        burn_cycles(PERIOD_1S_CYCLES);
         snprintf(buf, sizeof(buf), "[%lu ms] LOW %u Step 2\r\n", xTaskGetTickCount(), count);
         uart_outstring(buf);
-        burn_cycles(8000000);
+        burn_cycles(PERIOD_1S_CYCLES);
         snprintf(buf, sizeof(buf), "[%lu ms] LOW %u Step 3\r\n", xTaskGetTickCount(), count);
         uart_outstring(buf);
         count++;
-        burn_cycles(8000000);
-        vTaskDelay(pdMS_TO_TICKS(3000)); 
+        burn_cycles(PERIOD_1S_CYCLES);
+        xTaskDelayUntil(&start_ticks, frequency_ticks); 
     }   
 }
 
 void med_priority_task(void* pvParams) {
     char buf[64];
+    const TickType_t frequency_ticks = pdMS_TO_TICKS(2000);
+    TickType_t start_ticks = last_wake;
     uint32_t count = 0;
     while (1) {
         snprintf(buf, sizeof(buf), "[%lu ms] MED %u Step 1\r\n", xTaskGetTickCount(),  count);
         uart_outstring(buf);
-        burn_cycles(8000000);
+        burn_cycles(PERIOD_1S_CYCLES);
         snprintf(buf, sizeof(buf), "[%lu ms] MED %u Step 2\r\n", xTaskGetTickCount(), count);
         uart_outstring(buf);
-        burn_cycles(8000000);
+        burn_cycles(PERIOD_1S_CYCLES);
         snprintf(buf, sizeof(buf), "[%lu ms] MED %u Step 3\r\n", xTaskGetTickCount(), count);
         uart_outstring(buf);
         count++; 
-        burn_cycles(8000000);
-        vTaskDelay(pdMS_TO_TICKS(2000));
+        burn_cycles(PERIOD_1S_CYCLES);
+        xTaskDelayUntil(&start_ticks, frequency_ticks); 
     }   
 }
 
 void high_priority_task(void* pvParams) {
     char buf[64];
+    const TickType_t frequency_ticks = pdMS_TO_TICKS(1000);
+    TickType_t start_ticks = last_wake;
     uint32_t count = 0;
     while (1) {
         snprintf(buf, sizeof(buf), "[%lu ms] HIGH %u \r\n", xTaskGetTickCount(), count);
         uart_outstring(buf);
-        vTaskDelay(pdMS_TO_TICKS(1000)); 
+        xTaskDelayUntil(&start_ticks, frequency_ticks); 
         count++;
     }
 }
@@ -61,6 +69,7 @@ int main(void) {
     xTaskCreate(low_priority_task, "low priority", 256, NULL, 2, NULL);
     xTaskCreate(med_priority_task, "med priority", 256, NULL, 3, NULL);
     xTaskCreate(high_priority_task, "high priority", 256, NULL, 10, NULL);
+    last_wake = xTaskGetTickCount();
     vTaskStartScheduler();
     while (1) {
 
