4bcfda2be73ca74a06ceb76d4cd0966d907a414e
Vedant Jhawar <vedantj1203@gmail.com>
2026-05-07 23:18:27 -0400

    lab1: preemptive scheduling done

--- src/main.c
+++ src/main.c
@@ -3,17 +3,8 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include <stdint.h>
-#include "irq.h"
 
 TaskHandle_t task_handle;
-
-// void blinkLED(void* pvParams) {
-//     int16_t *delay = (int16_t*)pvParams;
-//     while (1) {
-//         GPIOA->ODR ^= (1 << 5); // flipping PA5
-//         vTaskDelay(pdMS_TO_TICKS(*delay)); 
-//     }
-// }
 
 void low_priority_task(void* pvParams) {
     int16_t *delay = (int16_t*)pvParams;
@@ -23,29 +14,33 @@ void low_priority_task(void* pvParams) {
     }   
 }
 
-void high_priority_task(void* pvParams) {
+void med_priority_task(void* pvParams) {
+    int16_t *delay = (int16_t*)pvParams;
     while (1) {
-        if (tim2_isr_flag) {
-            tim2_isr_flag = 0;
-            uart_outstring("HIGH\r\n");
-            xTaskNotify(task_handle, 0x01, eSetBits); 
-        }
+        uart_outstring("MED\r\n");
+        vTaskDelay(pdMS_TO_TICKS(*delay)); 
+    }   
+}
+
+void high_priority_task(void* pvParams) {
+    int16_t *delay = (int16_t*)pvParams;
+    while (1) {
+        uart_outstring("HIGH\r\n");
+        vTaskDelay(pdMS_TO_TICKS(*delay)); 
     }
 }
 
 int main(void) {
-    // int16_t blink_led_delay = 500;
     int16_t low_priority_delay = 100;
-    int8_t high_priority_delay = 50; 
+    int16_t med_priority_delay = 300;
+    int16_t high_priority_delay = 500; 
 
     PLL_Init();
     led2_Init();
     uart_Init();
-    timer2_init(high_priority_delay); 
-    timer2_start();
-    // xTaskCreate(blinkLED, "led blink", 256, (void*)&blink_led_delay, 5, NULL);
     xTaskCreate(low_priority_task, "low priority", 256, (void*)&low_priority_delay, 2, NULL);
-    xTaskCreate(high_priority_task, "high priority", 256, NULL, 5, NULL);
+    xTaskCreate(med_priority_task, "med priority", 256, (void*)&med_priority_delay, 4, NULL);
+    xTaskCreate(high_priority_task, "high priority", 256, (void*)&high_priority_delay, 6, NULL);
     vTaskStartScheduler();
     while (1) {
 
